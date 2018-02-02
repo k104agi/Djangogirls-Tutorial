955f7aa0f59927bc533dc73aec1c64f3220fa512
kjs <k104agi@gmail.com>
2018-02-02 10:22:28 +0900

    목요일에 못 왔고 금요일 아침에 업뎃

--- django/blog/models.py
+++ django/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
     author = models.ForeignKey(
         'auth.User',
@@ -8,12 +9,18 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
-    created_Date = models.DateTimeField(
+    created_date = models.DateTimeField(
         default=timezone.now
     )
     published_date = models.DateTimeField(
         blank=True, null=True
     )
+
+    class Meta:
+        verbose_name = '글'
+        verbose_name_plural = f'{verbose_name} 목록'
+        ordering = ['-created_date']
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
